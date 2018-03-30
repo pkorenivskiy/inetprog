db841a45772c22ef40f0bdd6f4a2dcca01f7fc73
Korenivskyi, Pavlo (US - Hermitage) <pkorenivskyi@deloitte.com>
2018-03-30 17:57:35 +0300

    html

--- code/lab3-2.js
+++ code/lab3-2.js
@@ -5,8 +5,6 @@ $(document).ready(function(){
 function btnLab32Run_Click() {
     var selector = $("#txtSel").val();
 
-    var ololo = $(selector);
-    console.log(ololo);
     console.log(selector);
     $(selector).css("background", randomColor());
 }
@@ -14,6 +12,5 @@ function btnLab32Run_Click() {
 function randomColor () {
     var back = ["#ff0000","blue","gray","red","green", "yellow"];
     var rand = back[Math.floor(Math.random() * back.length)];
-    //$('div').css('background',rand);
     return rand;
   };
